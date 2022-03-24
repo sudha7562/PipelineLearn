6bc8bf6ba68937e8c3a3ee42297238195cb1ab69
Vinayak Joshi <t-vjoshi@microsoft.com>
2022-03-24 13:59:19 +0530

    changes

---
+++ CodeCoverage/Module1Test1/Module1Test2UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Module1;
+
+namespace Module1Test1
+{
+    [TestClass]
+    public class Module1Test2UnitTest1
+    {
+        [TestMethod]
+        public void Module1Test2UnitTest1_TestMethod1()
+        {
+            var module1Class2 = new Module1Class2();
+            Assert.AreEqual(1, module1Class2.Module1Class2Method1(4));
+        }
+    }
+}
