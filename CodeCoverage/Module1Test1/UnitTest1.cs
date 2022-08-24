95746469fa6f92ed27f18bdef1b527517895bc8c
Vinayak Joshi <vinayakjoshi@microsoft.com>
2022-08-24 04:53:58 +0000

    Added 4 files to /CodeCoverage/Module1Test1

---
+++ CodeCoverage/Module1Test1/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject151
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
