6e484f35d2b9ed992a826ffc3564eec488135873
Vinayak Joshi <t-vjoshi@microsoft.com>
2022-08-26 05:55:48 +0000

    Updated UnitTest1.cs

--- CodeCoverage/Module1Test1/UnitTest1.cs
+++ CodeCoverage/Module1Test1/UnitTest1.cs
@@ -6,22 +6,37 @@ namespace UnitTestProject151
     [TestClass]
     public class UnitTest1
     {
+       public static bool Test1Called=false;
+        public static bool Test2Called=false;
+        public static bool Test3Called=false;
+
+     
+        
         [TestMethod]
         public void TestMethod2()
         {
-            Assert.IsTrue(true);
-        }
+            Test2Called = true;
 
-        [TestMethod]
-        public void TestMethod1()
-        {
-            Assert.IsTrue(true);
+            Assert.IsTrue(Test1Called);
+            Assert.IsFalse(Test3Called);
         }
 
         [TestMethod]
         public void TestMethod3()
         {
-            Assert.IsTrue(true);
+            Test3Called = true;
+
+            Assert.IsTrue(Test1Called);
+            Assert.IsTrue(Test2Called);
+        }
+
+            [TestMethod]
+        public void TestMethod1()
+        {
+            Test1Called = true;
+
+            Assert.IsFalse(Test2Called);
+            Assert.IsFalse(Test3Called);
         }
     }
 }
